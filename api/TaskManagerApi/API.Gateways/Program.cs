d138604dff1f07ebfa95d0f6e3de6ca79defd8b8
oalkabouss <omaralkabous@gmail.com>
2024-05-05 22:01:04 +0200

    Add authentication configuration to gateways

--- api/TaskManagerApi/API.Gateways/Program.cs
+++ api/TaskManagerApi/API.Gateways/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -6,9 +7,22 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false,  reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
+    {
+        o.MetadataAddress = "http://localhost:8080/identity/realms/secured/.well-known/openid-configuration";
+        o.RequireHttpsMetadata = false;
+        o.Authority = "http://localhost:8080/realms/secured";
+        o.Audience = "account";
+    });
+
 WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 await app.UseOcelot();
-app.Run();
+await app.RunAsync();
